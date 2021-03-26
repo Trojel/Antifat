35d802232ae5468d6ce813da46294866aab0a721
Bøsse <wtrojel@gmail.com>
2021-03-26 13:51:27 +0100

    http fix

--- websockets.js
+++ websockets.js
@@ -5,9 +5,16 @@ class Web
 
     }
 
-    test()
+    son()
     {
-        fetch("http://127.0.0.1:1880/test")
+        fetch("http://127.0.0.1:1880/son")
+        .then(data => {return data.json()})
+        .then(res => {console.log(res)})
+    }
+
+    soff()
+    {
+        fetch("http://127.0.0.1:1880/soff")
         .then(data => {return data.json()})
         .then(res => {console.log(res)})
     }
