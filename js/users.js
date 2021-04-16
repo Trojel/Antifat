611aa5ebc2d4e47f2c789f3d02d3d6a552752d98
OliverEC <oliverec04@gmail.com>
2021-04-16 13:43:45 +0200

    hej

--- js/users.js
+++ js/users.js
@@ -1,12 +1,5 @@
 let users;
 
-const response = await fetch(path);
-    const json = await response.json();
-    users = json;
-
-console.log("hej5")
-console.log(users)
-
 //for (let i = 0; i < json.users.length; i++){
 
 //}
