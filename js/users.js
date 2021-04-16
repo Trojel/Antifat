69df1f537e5dae862faeb39d07fa617e5e36da2a
Bøsse <wtrojel@gmail.com>
2021-04-16 12:40:37 +0200

    hej

--- js/users.js
+++ js/users.js
@@ -1,13 +1,14 @@
 json = fetch("database.json")
         .then (response => response.json())
-        .then (getJson(json))
+        .then (console.log(json.users.length))
 
 
-function getJson(data)
-{
-     for(let i = 0; i < data.users.length; i++){
-        console.log(i)
-    }   
-}
+
+// function getJson(data)
+// {
+//      for(let i = 0; i < data.users.length; i++){
+//         console.log(i)
+//     }   
+// }
 
 
