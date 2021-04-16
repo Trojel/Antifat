104145c908ee61094940e8cf4beaedc00d122a78
Bøsse <wtrojel@gmail.com>
2021-04-16 12:35:15 +0200

    lort

--- js/users.js
+++ js/users.js
@@ -1,11 +1,12 @@
-json = fetch("database.json")
-        .then(response => response.json())
-        .then(getJson(json))
+json = await fetch("database.json")
+        .then (response => response.json())
+        .then (getJson(json))
 
 
-function getJson(data){
-     for (let i = 0; i < data.users.length; i++){
-        console.log("hej")
+function getJson(data)
+{
+     for(let i = 0; i < data.users.length; i++){
+        console.log(i)
     }   
 }
 
